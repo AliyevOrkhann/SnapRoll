46119d6898d42d27df145f8a885b74c6f59ab6fb
Orkhan Aliyev <oaliyev18145@ada.edu.az>
2025-12-14 22:44:02 +0400

    database migration

--- src/SnapRoll.API/Program.cs
+++ src/SnapRoll.API/Program.cs
@@ -161,6 +161,22 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Auto-migrate database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SnapRollDbContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
